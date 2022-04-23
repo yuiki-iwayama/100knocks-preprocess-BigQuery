bed307185c844e64657a05b05c8e69743b22e2b3
yuiki-iwayama <yuiki.iwayama@gmail.com>
2022-04-24 03:37:22 +0900

    ディレクトリ指定などの軽微な修正

--- docker/init.sh
+++ docker/init.sh
@@ -13,42 +13,42 @@ bq --location=asia-northeast1 mk --dataset $GCP_PROJECT_ID:$GCP_DATA
 bq --location=asia-northeast1 load \
 --source_format=CSV --skip_leading_rows 1 \
 $GCP_PROJECT_ID:$GCP_DATA.customer \
-/work/100knocks/data/customer.csv \
+/work/data/customer.csv \
 customer_id:STRING,customer_name:STRING,gender_cd:STRING,gender:STRING,birth_date:DATE,age:INT64,postal_cd:STRING,address:STRING,application_store_cd:STRING,application_date:STRING,status_cd:STRING
 
 # category
 bq --location=asia-northeast1 load \
 --source_format=CSV --skip_leading_rows 1 \
 $GCP_PROJECT_ID:$GCP_DATA.category \
-/work/100knocks/data/category.csv \
+/work/data/category.csv \
 category_major_cd:STRING,category_major_name:STRING,category_medium_cd:STRING,category_medium_name:STRING,category_small_cd:STRING,category_small_name:STRING
 
 # product
 bq --location=asia-northeast1 load \
 --source_format=CSV --skip_leading_rows 1 \
 $GCP_PROJECT_ID:$GCP_DATA.product \
-/work/100knocks/data/product.csv \
+/work/data/product.csv \
 product_cd:STRING,category_major_cd:STRING,category_medium_cd:STRING,category_small_cd:STRING,unit_price:INT64,unit_cost:INT64
 
 # store
 bq --location=asia-northeast1 load \
 --source_format=CSV --skip_leading_rows 1 \
 $GCP_PROJECT_ID:$GCP_DATA.store \
-/work/100knocks/data/store.csv \
+/work/data/store.csv \
 store_cd:STRING,store_name:STRING,prefecture_cd:STRING,prefecture:STRING,address:STRING,address_kana:STRING,tel_no:STRING,longitude:NUMERIC,latitude:NUMERIC,floor_area:NUMERIC
 
 # receipt
 bq --location=asia-northeast1 load \
 --source_format=CSV --skip_leading_rows 1 \
 $GCP_PROJECT_ID:$GCP_DATA.receipt \
-/work/100knocks/data/receipt.csv \
+/work/data/receipt.csv \
 sales_ymd:INT64,sales_epoch:INT64,store_cd:STRING,receipt_no:INT64,receipt_sub_no:INT64,customer_id:STRING,product_cd:STRING,quantity:INT64,amount:INT64
 
 # geocode
 bq --location=asia-northeast1 load \
 --source_format=CSV --skip_leading_rows 1 \
 $GCP_PROJECT_ID:$GCP_DATA.geocode \
-/work/100knocks/data/geocode.csv \
+/work/data/geocode.csv \
 postal_cd:STRING,prefecture:STRING,city:STRING,town:STRING,street:STRING,address:STRING,full_address:STRING,longitude:NUMERIC,latitude:NUMERIC
 
 jupyter lab --ip=0.0.0.0 --allow-root --LabApp.token=''
