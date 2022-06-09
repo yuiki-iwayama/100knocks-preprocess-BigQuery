3ebe598bc8622120450668cab3e0e3b291cf72fd
Yuiki Iwayama <69156908+yuiki-iwayama@users.noreply.github.com>
2022-06-10 00:13:07 +0900

    毎回データセット作成するのをやめる

--- docker/init.sh
+++ docker/init.sh
@@ -2,54 +2,59 @@
 
 # GCPプロジェクトの設定
 gcloud auth activate-service-account $GCP_IAM --key-file=/run/secrets/user_gcp_key
-gcloud config set project $GCP_PROJECT_ID
-gcloud config set compute/region asia-northeast1
-gcloud config set compute/zone b
 
-# データをBigQueryへ流し込む
-bq rm -f -r $GCP_PROJECT_ID:$GCP_DATA
-bq --location=asia-northeast1 mk --dataset $GCP_PROJECT_ID:$GCP_DATA
+# すでにデータセットがあるかどうかの確認
+bq show --format=prettyjson $GCP_PROJECT_ID:$GCP_DATA | read -d '' mydata
+if [-z "$mydata"]; then
+  gcloud config set project $GCP_PROJECT_ID
+  gcloud config set compute/region asia-northeast1
+  gcloud config set compute/zone b
 
-# customer
-bq --location=asia-northeast1 load \
---source_format=CSV --skip_leading_rows 1 \
-$GCP_PROJECT_ID:$GCP_DATA.customer \
-/home/jovyan/work/data/customer.csv \
-customer_id:STRING,customer_name:STRING,gender_cd:STRING,gender:STRING,birth_day:DATE,age:INT64,postal_cd:STRING,address:STRING,application_store_cd:STRING,application_date:STRING,status_cd:STRING
+  # データをBigQueryへ流し込む
+  bq rm -f -r $GCP_PROJECT_ID:$GCP_DATA
+  bq --location=asia-northeast1 mk --dataset $GCP_PROJECT_ID:$GCP_DATA
 
-# category
-bq --location=asia-northeast1 load \
---source_format=CSV --skip_leading_rows 1 \
-$GCP_PROJECT_ID:$GCP_DATA.category \
-/home/jovyan/work/data/category.csv \
-category_major_cd:STRING,category_major_name:STRING,category_medium_cd:STRING,category_medium_name:STRING,category_small_cd:STRING,category_small_name:STRING
+  # customer
+  bq --location=asia-northeast1 load \
+  --source_format=CSV --skip_leading_rows 1 \
+  $GCP_PROJECT_ID:$GCP_DATA.customer \
+  /home/jovyan/work/data/customer.csv \
+  customer_id:STRING,customer_name:STRING,gender_cd:STRING,gender:STRING,birth_day:DATE,age:INT64,postal_cd:STRING,address:STRING,application_store_cd:STRING,application_date:STRING,status_cd:STRING
 
-# product
-bq --location=asia-northeast1 load \
---source_format=CSV --skip_leading_rows 1 \
-$GCP_PROJECT_ID:$GCP_DATA.product \
-/home/jovyan/work/data/product.csv \
-product_cd:STRING,category_major_cd:STRING,category_medium_cd:STRING,category_small_cd:STRING,unit_price:INT64,unit_cost:INT64
+  # category
+  bq --location=asia-northeast1 load \
+  --source_format=CSV --skip_leading_rows 1 \
+  $GCP_PROJECT_ID:$GCP_DATA.category \
+  /home/jovyan/work/data/category.csv \
+  category_major_cd:STRING,category_major_name:STRING,category_medium_cd:STRING,category_medium_name:STRING,category_small_cd:STRING,category_small_name:STRING
 
-# store
-bq --location=asia-northeast1 load \
---source_format=CSV --skip_leading_rows 1 \
-$GCP_PROJECT_ID:$GCP_DATA.store \
-/home/jovyan/work/data/store.csv \
-store_cd:STRING,store_name:STRING,prefecture_cd:STRING,prefecture:STRING,address:STRING,address_kana:STRING,tel_no:STRING,longitude:NUMERIC,latitude:NUMERIC,floor_area:NUMERIC
+  # product
+  bq --location=asia-northeast1 load \
+  --source_format=CSV --skip_leading_rows 1 \
+  $GCP_PROJECT_ID:$GCP_DATA.product \
+  /home/jovyan/work/data/product.csv \
+  product_cd:STRING,category_major_cd:STRING,category_medium_cd:STRING,category_small_cd:STRING,unit_price:INT64,unit_cost:INT64
 
-# receipt
-bq --location=asia-northeast1 load \
---source_format=CSV --skip_leading_rows 1 \
-$GCP_PROJECT_ID:$GCP_DATA.receipt \
-/home/jovyan/work/data/receipt.csv \
-sales_ymd:INT64,sales_epoch:INT64,store_cd:STRING,receipt_no:INT64,receipt_sub_no:INT64,customer_id:STRING,product_cd:STRING,quantity:INT64,amount:INT64
+  # store
+  bq --location=asia-northeast1 load \
+  --source_format=CSV --skip_leading_rows 1 \
+  $GCP_PROJECT_ID:$GCP_DATA.store \
+  /home/jovyan/work/data/store.csv \
+  store_cd:STRING,store_name:STRING,prefecture_cd:STRING,prefecture:STRING,address:STRING,address_kana:STRING,tel_no:STRING,longitude:NUMERIC,latitude:NUMERIC,floor_area:NUMERIC
 
-# geocode
-bq --location=asia-northeast1 load \
---source_format=CSV --skip_leading_rows 1 \
-$GCP_PROJECT_ID:$GCP_DATA.geocode \
-/home/jovyan/work/data/geocode.csv \
-postal_cd:STRING,prefecture:STRING,city:STRING,town:STRING,street:STRING,address:STRING,full_address:STRING,longitude:NUMERIC,latitude:NUMERIC
+  # receipt
+  bq --location=asia-northeast1 load \
+  --source_format=CSV --skip_leading_rows 1 \
+  $GCP_PROJECT_ID:$GCP_DATA.receipt \
+  /home/jovyan/work/data/receipt.csv \
+  sales_ymd:INT64,sales_epoch:INT64,store_cd:STRING,receipt_no:INT64,receipt_sub_no:INT64,customer_id:STRING,product_cd:STRING,quantity:INT64,amount:INT64
+
+  # geocode
+  bq --location=asia-northeast1 load \
+  --source_format=CSV --skip_leading_rows 1 \
+  $GCP_PROJECT_ID:$GCP_DATA.geocode \
+  /home/jovyan/work/data/geocode.csv \
+  postal_cd:STRING,prefecture:STRING,city:STRING,town:STRING,street:STRING,address:STRING,full_address:STRING,longitude:NUMERIC,latitude:NUMERIC
+fi
 
 start-notebook.sh --NotebookApp.token=''
